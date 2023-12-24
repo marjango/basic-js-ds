a7ab82d86434774e4c89779e42a9780198d9baf6
marjango <misoo00up@gmail.com>
2023-12-24 20:20:38 +0600

    feat: update

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,108 +1,128 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
-
   constructor() {
-    this._root = null;
+    this.rootNode = null;
   }
 
   root() {
-    return this._root;
+    return this.rootNode;
   }
 
   add(data) {
-    this._root = this._insert(this._root, data);
-}
-
-_insert(node, data) {
-  if (!node) {
-    return new Node(data);
+    this.rootNode = this._insert(this.rootNode, data);
   }
 
-  if (data < node.data) {
-    node.left = this._insert(node.left, data);
-  } else if (data > node.data) {
-    node.right = this._insert(node.right, data);
-  }
+  _insert(node, data) {
+    if (node === null) {
+      return new Node(data);
+    }
 
-  return node;
+    if (data < node.data) {
+      node.left = this._insert(node.left, data);
+    } else if (data > node.data) {
+      node.right = this._insert(node.right, data);
+    }
+
+    return node;
   }
 
   has(data) {
-    return this._search(this._root, data) !== null;
+    return this._search(this.rootNode, data);
   }
-  
+
   _search(node, data) {
-    if (!node || node.data === data) {
-      return node;
+    if (node === null) {
+      return false;
     }
-  
-    if (data < node.data) {
+
+    if (data === node.data) {
+      return true;
+    } else if (data < node.data) {
       return this._search(node.left, data);
     } else {
       return this._search(node.right, data);
     }
-  }  
+  }
 
   find(data) {
-    return this._search(this._root, data);
+    return this._find(this.rootNode, data);
   }
-  
-  remove(data) {
-    this._root = this._remove(this._root, data);
-  }
-  
-  _remove(node, data) {
-    if (!node) {
+
+  _find(node, data) {
+    if (node === null) {
       return null;
     }
-  
-    if (data < node.data) {
-      node.left = this._remove(node.left, data);
-    } else if (data > node.data) {
-      node.right = this._remove(node.right, data);
+
+    if (data === node.data) {
+      return node;
+    } else if (data < node.data) {
+      return this._find(node.left, data);
     } else {
-      if (!node.left) {
+      return this._find(node.right, data);
+    }
+  }
+
+  remove(data) {
+    this.rootNode = this._remove(this.rootNode, data);
+  }
+
+  _remove(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data === node.data) {
+      // Node with only one child or no child
+      if (node.left === null) {
         return node.right;
-      } else if (!node.right) {
+      } else if (node.right === null) {
         return node.left;
       }
-  
-      node.data = this._findMin(node.right).data;
+
+      // Node with two children
+      node.data = this._getMinValue(node.right);
       node.right = this._remove(node.right, node.data);
+    } else if (data < node.data) {
+      node.left = this._remove(node.left, data);
+    } else {
+      node.right = this._remove(node.right, data);
     }
-  
+
     return node;
   }
-  
-  _findMin(node) {
-    while (node.left) {
-      node = node.left;
+
+  _getMinValue(node) {
+    let current = node;
+    while (current.left !== null) {
+      current = current.left;
     }
-    return node;
-  }  
+    return current.data;
+  }
 
   min() {
-    return this._findMin(this._root).data;
+    return this._getMinValue(this.rootNode);
   }
-  
+
   max() {
-    return this._findMax(this._root).data;
+    return this._getMaxValue(this.rootNode);
   }
-  
-  _findMax(node) {
-    while (node.right) {
-      node = node.right;
+
+  _getMaxValue(node) {
+    let current = node;
+    while (current.right !== null) {
+      current = current.right;
     }
-    return node;
+    return current.data;
   }
-  
 }
 
 module.exports = {
